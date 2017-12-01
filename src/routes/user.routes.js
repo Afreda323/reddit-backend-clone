6372c3a06961ed8597e54681832fd35a0ee5fb5d
Afreda323 <anthonyfreda323@gmail.com>
2017-11-30 21:30:03 -0500

    Protected routes made

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,12 +1,18 @@
 const Router = require('koa-router')
+const jwt = require('koa-jwt')
 const userController = require('../controllers/user.controller')
 
 const userRouter = new Router()
 const controller = new userController()
 
-userRouter.get('/:id', controller.getUser)
 userRouter.post('/signup', controller.signupUser)
 userRouter.post('/login', controller.loginUser)
 
+// Protect next routes
+userRouter.use(jwt({ secret: process.env.SECRET }))
+
+userRouter.get('/secrets', ctx => {
+  ctx.body = 'you made it'
+})
 
 module.exports = userRouter
