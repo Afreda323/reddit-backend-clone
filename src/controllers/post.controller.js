168ee84293008a9e6d7eeb8ff8780100972e1ccf
Afreda323 <anthonyfreda323@gmail.com>
2017-12-10 10:52:07 -0500

    Can fetch a users subscribers posts

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -19,6 +19,7 @@ module.exports = class PostController {
     this.getPostsByCommunity = this.getPostsByCommunity.bind(this)
     this.upvote = this.upvote.bind(this)
     this.downvote = this.downvote.bind(this)
+    this.getUserSubs = this.getUserSubs.bind(this)
   }
   async createPost(ctx) {
     const { community, user, title, content } = ctx.request.body
@@ -30,11 +31,11 @@ module.exports = class PostController {
     ctx.assert(community && isMongoId(community), 'Enter a valid community id')
     ctx.assert(
       title && typeof title === 'string' && title.length > 3,
-      'Enter a title',
+      'Enter a title'
     )
     ctx.assert(
       content && typeof content === 'string' && content.length > 3,
-      'Enter some content',
+      'Enter some content'
     )
     // Verify community exists
     const dbCommunity = await this.communityService.getCommunity(community)
@@ -89,14 +90,14 @@ module.exports = class PostController {
     if (title) {
       ctx.assert(
         title && typeof title === 'string' && title.length > 3,
-        'Enter a title',
+        'Enter a title'
       )
       q.title = title
     }
     if (content) {
       ctx.assert(
         content && typeof content === 'string' && content.length > 3,
-        'Enter some content',
+        'Enter some content'
       )
       q.text = content
     }
@@ -185,5 +186,31 @@ module.exports = class PostController {
     ctx.body = res
   }
   // TODO
-  async getUserSubs(ctx) {}
+  async getUserSubs(ctx) {
+    const { id } = ctx.params
+    const { skip } = ctx.query
+    ctx.assert(isMongoId(id), 'Enter a valid ID')
+
+    const q = { skip: 0 }
+    if (skip) {
+      q.skip = parseInt(skip)
+    }
+    // Fetch the user
+    const user = await this.userService.getUser(id)
+    // Fetch all user subscriptions
+    const subs = await Promise.all(
+      user.subscriptions.map(
+        async sub => await this.communityService.getCommunity(sub)
+      )
+    )
+    // Strip all of the posts off the subscription
+    // And slice for desired skip, paginated by 10
+    const posts = subs
+      .reduce((a, b) => [...a, ...b.posts], [])
+      .slice(q.skip, q.skip + 10)
+    const response = await Promise.all(
+      posts.map(async post => await this.postService.getPost(post))
+    )
+    ctx.body = response
+  }
 }
